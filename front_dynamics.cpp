74ed89c5bc5b6e344bcbd2bc33602e834f95e35f
fredfarrell <ffarrell123@gmail.com>
2013-12-16 15:01:30 +0000

    Updated to use 3rd order upwind differencing scheme

--- front_dynamics.cpp
+++ front_dynamics.cpp
@@ -26,7 +26,7 @@ void init() {
 	for(int i=0;i<SIZE;i++) {
 
 		rx.push_back(i);
-		ry.push_back(100*exp(-(i-500)*(i-500)/1000.0));
+		ry.push_back(1000*exp(-(i-500)*(i-500)/1000.0));
 		//ry.push_back(0);
 
 		deltarx.push_back(0);
@@ -57,6 +57,20 @@ void print(int j) {
 }
 
 
+//functions to calculate i+1 and i-1 with PBCs
+int up(int x) {
+
+	if(x==rx.size()-1) return 0;
+	else return x+1;
+}
+
+int dwn(int x) {
+
+	if(x==0) return rx.size()-1;
+	else return x-1;
+
+}
+
 
 void timestep(int tm) {
 
@@ -64,30 +78,28 @@ void timestep(int tm) {
 	for(int i=0;i<rx.size();i++) {
 
 
-		int iup = (i==rx.size()-1)?0:i+1;
-		int idwn = (i==0)?rx.size()-1:i-1;
+		//calculate positions of points 1 or 2 up or down from current cell using minimum image
+		//sees if the difference between the current cell and (e.g.) i+1 is > SIZE/2, returns a shifted point if so
+		double rxup,rxdwn,rx2up,rx2dwn;
+		rxup=(rx[up(i)]-rx[i]<-SIZE*0.5)?rx[up(i)]+SIZE:rx[up(i)]; 
+		rxdwn=(rx[dwn(i)]-rx[i]>SIZE*0.5)?rx[dwn(i)]-SIZE:rx[dwn(i)]; 
+		//rx2up=(rx[up(up(i))]-rx[i]<-SIZE*0.5)?rx[up(up(i))]+SIZE:rx[up(up(i))];  not actually used
+		rx2dwn=(rx[dwn(dwn(i))]-rx[i]>SIZE*0.5)?rx[dwn(dwn(i))]-SIZE:rx[dwn(dwn(i))]; 
 
-		double drx = rx[i]-rx[idwn]; //v is +ve for delta-rx
-		if (fabs(drx)>SIZE*0.5) drx = drx - SIZE*SGN(drx); //minimum image for PBCs
+		double drx = (2*rxup + 3*rx[i] - 6*rxdwn + 2*rx2dwn)/6.0; //3rd order upwind scheme w/ different sign for the two dimensions
+		double dry = (-ry[up(up(i))] + 6*ry[up(i)] - 3*ry[i] - 2*ry[dwn(i)])/6.0; 
 
-		double dry = ry[iup]-ry[i]; //v is -ve for delta-ry
 		double norm = sqrt(drx*drx+dry*dry);
-
-		//cout<<i<<" "<<tm<<" "<<drx<<" "<<dry<<" "<<norm<<endl;
 
 		drx/=norm; dry/=norm;
 
 		deltarx[i] =  - dt*v*dry/dx; //update with upwind scheme
 		deltary[i] =    dt*v*drx/dx;
 
-
 	}
 
 
 	for(int i=0;i<rx.size();i++) { //move the points
-
-		int iup = (i==rx.size()-1)?0:i+1;
-		int idwn = (i==0)?rx.size()-1:i-1; 
 
 		rx[i]+=deltarx[i]; //update with upwind scheme
 		ry[i]+=deltary[i];
@@ -109,7 +121,7 @@ int main() {
 	init();
 	print(1000);
 
-	for (int i=0;i<50000;i++) {
+	for (int i=0;i<5000;i++) {
 		
 		timestep(i);
 		print(i);
