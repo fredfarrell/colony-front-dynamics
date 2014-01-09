ed6ce04b0193c3ce4c50663632b88173b11c54df
fredfarrell <ffarrell123@gmail.com>
2014-01-09 16:42:29 +0000

    Added variable v

--- front_dynamics.cpp
+++ front_dynamics.cpp
@@ -15,11 +15,18 @@ using namespace std;
 vector<double> rx,ry,deltarx,deltary;
 double dt=0.01;
 double ds=1.0;
-double v=1.0;
+double v0=1.0;
 double D=0.0;
 double ymin,ymax;
 
 
+
+double v(double x) {
+
+	if(x>300 && x<700) return v0 + 0.05*exp( -(x-500)*(x-500)/200 );
+	else return v0;
+
+}
 
 void init() {
 
@@ -27,8 +34,8 @@ void init() {
 	for(int i=0;i<SIZE;i++) {
 
 		rx.push_back(i);
-		ry.push_back(100*exp(-(i-500)*(i-500)/1000.0));
-		//ry.push_back(0);
+		//ry.push_back(100*exp(-(i-500)*(i-500)/1000.0));
+		ry.push_back(0);
 
 		deltarx.push_back(0);
 		deltary.push_back(0);		
@@ -85,7 +92,7 @@ void timestep(int tm) {
 		rxup=(rx[up(i)]-rx[i]<-SIZE*0.5)?rx[up(i)]+SIZE:rx[up(i)]; 
 		rxdwn=(rx[dwn(i)]-rx[i]>SIZE*0.5)?rx[dwn(i)]-SIZE:rx[dwn(i)]; 
 		//rx2up=(rx[up(up(i))]-rx[i]<-SIZE*0.5)?rx[up(up(i))]+SIZE:rx[up(up(i))];  not actually used
-		rx2dwn=(rx[dwn(dwn(i))]-rx[i]>SIZE*0.5)?rx[dwn(dwn(i))]-SIZE:rx[dwn(dwn(i))]; 
+		//rx2dwn=(rx[dwn(dwn(i))]-rx[i]>SIZE*0.5)?rx[dwn(dwn(i))]-SIZE:rx[dwn(dwn(i))]; ditto
 
 		double drxds = (rxup - rxdwn)/(2.0*ds); //centred difference 
 		double dryds = (ry[up(i)] - ry[dwn(i)])/(2.0*ds); 
@@ -97,8 +104,8 @@ void timestep(int tm) {
 
 		drxds/=norm; dryds/=norm;
 
-		deltarx[i] =  - dt*v*dryds + D*dt*d2rxds2;
-		deltary[i] =    dt*v*drxds + D*dt*d2ryds2;
+		deltarx[i] =  - dt*v(i)*dryds + D*dt*d2rxds2;
+		deltary[i] =    dt*v(i)*drxds + D*dt*d2ryds2;
 
 	}
 
@@ -125,10 +132,10 @@ int main() {
 	init();
 	print(1000);
 
-	for (int i=0;i<5000;i++) {
+	for (int i=0;i<50000;i++) {
 		
 		timestep(i);
-		if(i%100==0) print(i);
+		if(i%1000==0) print(i);
 	}
 
 
