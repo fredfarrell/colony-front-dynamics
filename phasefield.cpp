225930455604f55fa3a83f17c0a0f574060a9912
fredfarrell <ffarrell123@gmail.com>
2014-01-10 17:37:21 +0000

    Added routine to shift the interface down as it moves

--- phasefield.cpp
+++ phasefield.cpp
@@ -1,3 +1,5 @@
+//code to solve the equation d\phi/dt = v |grad \phi| in 2D
+
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
@@ -15,6 +17,8 @@ double **phi,**dphi;
 double dt=0.01;
 double dx=0.1;
 double v=1.0;
+
+int totalshift=0;
 
 void init() {
 
@@ -34,7 +38,7 @@ void init() {
 
 }
 
-int up(int x) {
+int up(int x) { //functions for PBCs
 
 	if(x==SIZE-1) return 0;
 	else return x+1;
@@ -53,7 +57,7 @@ void timestep() {
 	for(int i=0;i<SIZE;i++) {
 		for(int j=1;j<SIZE-1;j++) {
 	
-			double dphidx = (phi[up(i)][j]-phi[dwn(i)][j])/(2*dx);
+			double dphidx = (phi[up(i)][j]-phi[dwn(i)][j])/(2*dx); //use FTCS scheme
 			double dphidy = (phi[i][j+1]-phi[i][j-1])/(2*dx);
 
 			dphi[i][j] = -dt*v*sqrt(dphidx*dphidx+dphidy*dphidy);
@@ -69,11 +73,43 @@ void timestep() {
 	
 }
 
+void shiftEverythingDown() { //shift everything to keep the interface roughly centred in the simulation
+
+    double tolerance = 0.01;
+    int ymin=SIZE;
+    int buffer=100; //number of lattice sites between the bottom of the contour and the bottom of the sim
+    
+    for(int i=0;i<SIZE;i++) {
+        for(int j=0;j<SIZE;j++) {
+            
+            if(fabs(phi[i][j])<tolerance && j<ymin) ymin=j;
+            
+        }
+    }
+    
+    int shift = ymin-buffer;
+    
+    if(shift<0) {
+        cout<<"Error: hitting bottom of sim box!"<<endl;
+        exit(0);
+    }
+            
+    for(int i=0;i<SIZE;i++) {
+        for(int j=0;j<SIZE;j++) {
+            
+            int x=j+shift;
+            if(x>=SIZE) phi[i][j]=1;
+            else phi[i][j]=phi[i][j+shift];
+    
+        }
+    }
+    
+}
+
 void printgrid(int t) {
 
 	char str[30];
 	sprintf(str,"out%i.dat",t);
-
 	ofstream outp(str);
 
 	for(int i=1;i<SIZE-1;i++) {
@@ -90,9 +126,11 @@ int main() {
 	init();
 	printgrid(1);
 
-	for(int i=0;i<1000;i++) {
+	for(int i=0;i<900;i++) {
 		
+		cout<<i<<endl;
 		timestep();
+		shiftEverythingDown();
 	
 	}
 
